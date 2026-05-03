beb6461d38dc8eecb208c019f5cb7c7f0630deda
Abhishek Tiwari <abhi1931480@gmail.com>
2026-05-03 12:19:04 +0530

    logs

--- modules/glue/main.tf
+++ modules/glue/main.tf
@@ -1,14 +1,6 @@
 locals {
   warehouse_path = "s3://${var.warehouse_bucket_name}/warehouse/"
   script_key     = "artifacts/glue/csv_to_iceberg.py"
-  spark_conf = join(", ", [
-    "spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
-    "spark.sql.catalog.glue_catalog=org.apache.iceberg.spark.SparkCatalog",
-    "spark.sql.catalog.glue_catalog.warehouse=${local.warehouse_path}",
-    "spark.sql.catalog.glue_catalog.catalog-impl=org.apache.iceberg.aws.glue.GlueCatalog",
-    "spark.sql.catalog.glue_catalog.io-impl=org.apache.iceberg.aws.s3.S3FileIO",
-    "spark.sql.defaultCatalog=glue_catalog"
-  ])
 }
 
 resource "aws_glue_catalog_database" "this" {
@@ -57,7 +49,6 @@ resource "aws_glue_job" "this" {
     "--database_name"                    = var.database_name
     "--table_name"                       = var.table_name
     "--warehouse_path"                   = local.warehouse_path
-    "--conf"                             = local.spark_conf
   }
 
   tags = var.tags
