b618c0354aebb14293e994f969671eb8a1f09225
Abhishek Tiwari <abhi1931480@gmail.com>
2026-05-03 20:09:27 +0530

    jenkins

--- modules/glue/main.tf
+++ modules/glue/main.tf
@@ -49,6 +49,14 @@ resource "aws_glue_job" "this" {
     "--database_name"                    = var.database_name
     "--table_name"                       = var.table_name
     "--warehouse_path"                   = local.warehouse_path
+
+    "--conf" = join(" ", [
+      "spark.sql.extensions=org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
+      "--conf spark.sql.catalog.glue_catalog=org.apache.iceberg.spark.SparkCatalog",
+      "--conf spark.sql.catalog.glue_catalog.warehouse=${local.warehouse_path}",
+      "--conf spark.sql.catalog.glue_catalog.catalog-impl=org.apache.iceberg.aws.glue.GlueCatalog",
+      "--conf spark.sql.catalog.glue_catalog.io-impl=org.apache.iceberg.aws.s3.S3FileIO"
+    ])
   }
 
   tags = var.tags
